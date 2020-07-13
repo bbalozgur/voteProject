485130d0ee613da99cb31d019fdcf7499c621224
özgür <obal@blutv.com>
2020-07-13 12:47:44 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Routers from './routers/routers';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Routers>
+      <App />
+    </Routers>
   </React.StrictMode>,
   document.getElementById('root')
 );
